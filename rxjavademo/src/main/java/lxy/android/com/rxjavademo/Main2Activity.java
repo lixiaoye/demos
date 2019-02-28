5abc289bc341cca0eeed0930ce4e38f9d78893be
lixiaoye <lixiaoye@kingsoft.com>
2019-03-01 00:23:04 +0800

    add null demo`:wq

---
+++ rxjavademo/src/main/java/lxy/android/com/rxjavademo/Main2Activity.java
@@ -0,0 +1,21 @@
+package lxy.android.com.rxjavademo;
+
+import android.app.Activity;
+import android.app.IntentService;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}

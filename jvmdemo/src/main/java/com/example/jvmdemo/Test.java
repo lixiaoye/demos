a7782a8c59fc27de02a1f48df4f3a1145ce3f612
lixiaoye <lixiaoye@kingsoft.com>
2019-04-16 11:24:47 +0800

    modify on develop

--- jvmdemo/src/main/java/com/example/jvmdemo/Test.java
+++ jvmdemo/src/main/java/com/example/jvmdemo/Test.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Method;
  * Created by lixiaoye on 2019/4/11.
  */
 public class Test {
+    
     public static void main(String[] args) {
         Student s1 = new Student();
         s1.setName("lixiaoye");

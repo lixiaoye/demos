73834f3412a98f4c0c8278126545fdfd72dc63ab
lixiaoye <lixiaoye@kingsoft.com>
2019-04-16 12:17:37 +0800

    modify on develop 10

--- jvmdemo/src/main/java/com/example/jvmdemo/Test.java
+++ jvmdemo/src/main/java/com/example/jvmdemo/Test.java
@@ -7,7 +7,6 @@ import java.lang.reflect.Method;
  * Created by lixiaoye on 2019/4/11.
  */
 public class Test {
-
     public static void main(String[] args) {
         
         Student s1 = new Student();
